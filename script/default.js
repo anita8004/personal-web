7c4f48f1ae72049f323d6a348c31dfaee9fb46a3
anita <anita8004@gmail.com>
2018-03-06 17:49:12 +0800

    進度更新20180306

--- script/default.js
+++ script/default.js
@@ -1,7 +1,18 @@
 $(function () {
+
+  /*hamburger*/
+
+  $(".hamburger").on("click", function (e) {
+      e.preventDefault();
+      $(this).toggleClass("open");
+      $("header .menu").toggleClass("open");
+  });
+
+
+
   new WOW().init();
 
   $('.parallax-window').each(function () {
     $(this).parallax();
-  })
+  });
 });
