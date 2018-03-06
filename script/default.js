965f9f58b6c19896374c311b752bc321e0c0f959
Yunhsiu <anita8004@gmail.com>
2018-03-06 17:49:12 +0800

    20180307更新
    Signed-off-by: anita <anita8004@gmail.com>

--- script/default.js
+++ script/default.js
@@ -1,18 +1,56 @@
 $(function () {
 
-  /*hamburger*/
+    /*
+    * 手機選單
+    */
 
-  $(".hamburger").on("click", function (e) {
-      e.preventDefault();
-      $(this).toggleClass("open");
-      $("header .menu").toggleClass("open");
-  });
+    $(".hamburger").on("click", function (e) {
+        e.preventDefault();
+        $(this).toggleClass("open");
+        var $menu = $("header .menu");
+        $menu.toggleClass("open");
+        if ($menu.hasClass("open")) {
+            $menu.css({
+                transform: "rotateX(0deg)",
+                transition: "transform .5s cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+            });
+        } else {
+            $menu.css({
+                transform: "rotateX(90deg)",
+                transition: "transform .5s cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+            });
+        }
+        ;
+    });
 
+    /*
+    * 桌機版消除css
+    * */
 
+    $(window).on("resize", function () {
+        var $menu = $("header .menu");
+        if (window.matchMedia("(min-width: 769px)")) {
+            $(".hamburger").removeClass("open");
+            $menu.removeClass("open");
+            $menu.css({
+                transform: "",
+                transition: ""
+            });
+        }
+        ;
+    }).trigger("resize");
 
-  new WOW().init();
+    /*
+    * wow網頁特效
+    * */
 
-  $('.parallax-window').each(function () {
-    $(this).parallax();
-  });
+    new WOW().init();
+
+    /*
+    * 視差滾動
+    * */
+
+    $('.parallax-window').each(function () {
+        $(this).parallax();
+    });
 });
