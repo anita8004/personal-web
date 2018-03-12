1e1aa8f76b2df4db1b8ec60c87a4cd6d06a9c5c8
anita <anita8004@gmail.com>
2018-03-12 17:31:57 +0800

    20180312更新
    新增特效，html,css修改，新增資料

--- script/default.js
+++ script/default.js
@@ -1,56 +1,71 @@
-$(function () {
-
-    /*
+$(function() {
+  /*
     * 手機選單
     */
 
-    $(".hamburger").on("click", function (e) {
-        e.preventDefault();
-        $(this).toggleClass("open");
-        var $menu = $("header .menu");
-        $menu.toggleClass("open");
-        if ($menu.hasClass("open")) {
-            $menu.css({
-                transform: "rotateX(0deg)",
-                transition: "transform .5s cubic-bezier(0.445, 0.05, 0.55, 0.95)"
-            });
-        } else {
-            $menu.css({
-                transform: "rotateX(90deg)",
-                transition: "transform .5s cubic-bezier(0.445, 0.05, 0.55, 0.95)"
-            });
-        }
-        ;
-    });
+  $(".hamburger").on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("open");
+    var $menu = $("header .menu");
+    $menu.toggleClass("open");
+    if ($menu.hasClass("open")) {
+      $menu.css({
+        transform: "rotateX(0deg)",
+        transition: "transform .5s cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+      });
+    } else {
+      $menu.css({
+        transform: "rotateX(90deg)",
+        transition: "transform .5s cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+      });
+    }
+  });
 
-    /*
+  /*
     * 桌機版消除css
     * */
 
-    $(window).on("resize", function () {
-        var $menu = $("header .menu");
-        if (window.matchMedia("(min-width: 769px)")) {
-            $(".hamburger").removeClass("open");
-            $menu.removeClass("open");
-            $menu.css({
-                transform: "",
-                transition: ""
-            });
-        }
-        ;
-    }).trigger("resize");
+  $(window)
+    .on("resize", function() {
+      var $menu = $("header .menu");
+      if (window.matchMedia("(min-width: 769px)")) {
+        $(".hamburger").removeClass("open");
+        $menu.removeClass("open");
+        $menu.css({ transform: "", transition: "" });
+      }
+    })
+    .trigger("resize");
 
-    /*
+  /*
     * wow網頁特效
     * */
 
-    new WOW().init();
+  new WOW().init();
 
-    /*
+  /*
     * 視差滾動
     * */
 
-    $('.parallax-window').each(function () {
-        $(this).parallax();
-    });
+  $(".parallax-window").each(function() {
+    $(this).parallax();
+  });
+
+  /**
+   * Isotope filter
+   * https://isotope.metafizzy.co/filtering.html
+   */
+
+  var $grid = $("#worksList").isotope({
+    itemSelector: ".item",
+    layoutMode: "masonry",
+    cellsByRow: {
+        columnWidth: 10   
+    },
+  });
+
+  $('.filters-button-group').on( 'click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
 });
